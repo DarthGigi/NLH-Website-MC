28800168eff7dfc92a91f16b97cf8ad4f337e55f
Gigi <mrgigithedev@gmail.com>
2022-01-03 16:30:50 +0100

    Initial Release of Redesign v0.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  content: ["*.html"],
+  content: ["*.html", "pages/*.html"],
   theme: {
     fontFamily: {
-      'Phospate': ['Phosphate'],
+      'Phospate': ['Phosphate, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif'],
     },
     extend: {},
   },
