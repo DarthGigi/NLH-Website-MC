48215cf9eae4d3cda56df01b9b82014db9f04328
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2022-01-07 22:58:59 +0100

    Initial Release of Redesign v1

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,5 +1,17 @@
-var element = document.querySelector("#tour");
-// smooth scroll to element and align it at the start
 function startTour() {
-    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    var element = document.querySelector("#tour");
+    // smooth scroll to element and align it at the start
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+}
+
+function mobileNavbar() {
+    var x = document.getElementById("mobileNav");
+    if (x.classList.contains("hidden")) {
+        x.classList.remove("hidden");
+    } else {
+        x.classList.add("hidden");
+    }
 }
