164de4dcb3c794aafdb03313d076e8c2e27cb606
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-04-07 21:05:54 +0200

    Prettier formatting

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,17 +1,17 @@
 function startTour() {
-    var element = document.querySelector("#tour");
-    // smooth scroll to element and align it at the start
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+  var element = document.querySelector("#tour");
+  // smooth scroll to element and align it at the start
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 }
 
 function mobileNavbar() {
-    var x = document.getElementById("mobileNav");
-    if (x.classList.contains("hidden")) {
-        x.classList.remove("hidden");
-    } else {
-        x.classList.add("hidden");
-    }
+  var x = document.getElementById("mobileNav");
+  if (x.classList.contains("hidden")) {
+    x.classList.remove("hidden");
+  } else {
+    x.classList.add("hidden");
+  }
 }
